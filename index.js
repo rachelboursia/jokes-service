fcbc1878253d9658ad8d6be365b1b74309644097
Rachel Boursia <91563448+rachelboursia@users.noreply.github.com>
2023-04-26 13:28:16 -0700

    adds /jokes routes

--- index.js
+++ index.js
@@ -7,14 +7,78 @@ app.use(express.urlencoded({extended:true}));
 
 app.get('/jokes', async (req, res, next) => {
   try {
-    // TODO - filter the jokes by tags and content
-    const jokes = [];
+ 
+ 
+    const tags = req.query.tags;
+    const content = req.query.content;
+ 
+ 
+    let jokes = await Joke.findAll();
+ 
+ 
+    if (tags) {
+      jokes = jokes.filter(j => j.tags.includes(tags));
+    }
+ 
+ 
+    if (content) {
+      jokes = jokes.filter(j => j.joke.includes(content));
+    }
+ 
+ 
     res.send(jokes);
+ 
+ 
   } catch (error) {
+ 
+ 
     console.error(error);
     next(error)
+ 
+ 
   }
-});
+ });
+ 
+ 
+ app.get('/jokes/:tags', async (req, res, next) => {
+  try {
+ 
+ 
+    let jokes = await Joke.findAll({ where: { tags: req.params.tags } });
+    res.send(jokes);
+ 
+ 
+  } catch (error) {
+ 
+ 
+    console.error(error);
+    next(error)
+ 
+ 
+  }
+ });
+ 
+ 
+ app.get('/jokes/:content', async (req, res, next) => {
+  try {
+ 
+ 
+    let jokes = await Joke.findAll();
+    jokes = jokes.filter(joke => joke.includes(req.params.content));
+    res.send(jokes);
+ 
+ 
+  } catch (error) {
+ 
+ 
+    console.error(error);
+    next(error)
+ 
+ 
+  }
+ });
+ 
+ 
 
 // we export the app, not listening in here, so that we can run tests
 module.exports = app;
